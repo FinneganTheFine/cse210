86f356fd3037c55a371729478ef98ee238490aa5
finneganthefine <finneganthefine@gmail.com>
2023-01-12 14:54:07 -0700

    Prep 4 finally worked

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Do you wanna play a game? yes or no");
+        string gameplay = Console.ReadLine() ?? "";
+        while (gameplay.ToLower() == "yes"){
+            Random generator = new Random();
+            int MagicNumber = generator.Next(1,101);
+            int Guesses = 0;
+            Console.Write("What do you think it is? ");
+            string StringGuess = Console.ReadLine() ?? "";
+            int Guess = int.Parse(StringGuess);
+            Guesses += 1;
+            while(Guess != MagicNumber){
+                if (Guess < MagicNumber){
+                    Console.WriteLine("Higher");
+                    StringGuess = Console.ReadLine() ?? "";
+                    Guesses += 1;
+                    Guess = int.Parse(StringGuess);
+                } else if (Guess > MagicNumber){
+                    Console.WriteLine("Lower");
+                    StringGuess = Console.ReadLine() ?? "";
+                    Guess = int.Parse(StringGuess);
+                    Guesses += 1;
+                }
+            }
+            Console.WriteLine("Wait you actually got it!?");
+            Console.WriteLine($"It took you {Guesses} Guesses to get it.");
+            Console.WriteLine();
+            Console.WriteLine("Do you wanna play a game? yes or no ");
+            gameplay = Console.ReadLine() ?? "";
+        }
+        Console.WriteLine("Okay...I understand");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Write the numbers that you want in the list. When you're finished type 0. ");
+        SortedList<int, int> numbers = new SortedList<int, int>();
+        Console.Write("Write your Number here: ");
+        string StringNumbers = Console.ReadLine() ?? "";
+        int numberinos = int.Parse(StringNumbers);
+        while (numberinos != 0){
+            if(numbers.ContainsKey(numberinos)){
+                Console.WriteLine("That's already here.");
+                Console.Write("Write your Number here: ");
+                StringNumbers = Console.ReadLine() ?? "";
+                numberinos = int.Parse(StringNumbers);
+            } else {
+                numbers.Add(numberinos, numberinos);
+                Console.Write("Write your Number here: ");
+                StringNumbers = Console.ReadLine() ?? "";
+                numberinos = int.Parse(StringNumbers);
+            }
+        }
+        int Total = 0;
+        int Average = 0;
+        int Maximum = -1;
+        int Min = 3333333;
+        foreach(var kvp in numbers){
+            if(kvp.Value > 0){
+                Total += kvp.Value;
+            }
+            if(kvp.Value > Maximum){
+                Maximum = kvp.Value;
+            }
+            if(kvp.Value > 0 && kvp.Value < Min){
+                Min = kvp.Value;
+            }
+        }
+        Average = Total/numbers.Count;
+        Console.WriteLine($"The total of your list is {Total}");
+        Console.WriteLine($"The average of your list is {Average}");
+        Console.WriteLine($"The largest number on your list is {Maximum}");
+        Console.WriteLine($"The smallest positive number on your list is {Min}");
+        foreach(var kvp in numbers){
+            Console.WriteLine(kvp);
+        }
     }
 }
