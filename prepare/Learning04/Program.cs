b8dac47409a8c70fda122abd077fa68ba591b043
finneganthefine <finneganthefine@gmail.com>
2023-02-15 21:18:10 -0700

    Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        var a = new Assignment("ryan", "Math");
+        var m = new MathAssignment("ryan", "Math", "Section 7.3", "Problems 8-19");
+        var w = new WritingAssignment("ryan", "Math", "The Causes of World War II");
+        Console.WriteLine(a.GetSummary());
+        Console.WriteLine(m.GetHomeworkList());
+        Console.WriteLine(w.GetWritingInformation());
+    }
+}
+
+class Assignment{
+    private string _studentName;
+    private string _topic;
+    public Assignment(string studentName, string topic){
+        _studentName = studentName;
+        _topic = topic;
+    }
+    public string GetSummary(){
+        string summary = $"Name: {_studentName} Topic: {_topic}";
+        return summary;
+    }
+}
+class MathAssignment: Assignment{
+    private string _textbookSection;
+    private string _problems;
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems): base(studentName, topic){
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+    public string GetHomeworkList(){
+        string homeworkList = $"{_textbookSection} {_problems}";
+        return homeworkList;
+    }
+}
+class WritingAssignment: Assignment{
+    private string _title;
+    public WritingAssignment(string studentName, string topic, string title):base(studentName, topic){
+        _title = title;
+    }
+    public string GetWritingInformation(){
+        string writingInformation = $"{_title}";
+        return writingInformation;
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,56 +1,18 @@
 ﻿class Program {
     static void Main(string[] args) {
-        
-        var divider =  new IntegerDivision();
-        divider.setTop(10);
-        divider.setRHS(1);
-        divider.DisplayResult();
+        var p = new Person();
+        var s = new Student();
 
-        divider.setTop(0);
-        divider.setRHS(100);
-        divider.DisplayResult();
-
-        divider.setTop(5);
-        divider.setRHS(0);
-        divider.DisplayResult();
-
-        divider.setTop(25);
-        divider.setRHS(5);
-        divider.DisplayResult();
+        Console.WriteLine(p.GetName());
+        Console.WriteLine(s.GetName());
     }
 }
 
-class IntegerDivision {
-    private int lhs = 1;
-    private int rhs = 1;
-    public int Result() {
-        return lhs / rhs;
+class Person{
+    public string GetName(){
+        return "Cole";
     }
-    public int getTop(){
-        return lhs;
-    }
-    public void setTop(int top){
-        lhs = top;
-    }
-    public int getRHS(){
-        return rhs;
-    }
-    public void setRHS(int top){
-        if(top == 0){
-            Console.WriteLine("Dividing by zero. Pick a different number");
-            string ryan = Console.ReadLine();
-            int newNum = int.Parse(ryan);
-            rhs = newNum;
-        }else{
-            rhs = top;
-        }
-    }
-    public void DisplayResult() {
-        if(rhs == 0){
-            Console.WriteLine("NO, Stop that");
-        }else{
-            var result = Result();
-            Console.WriteLine($"{lhs} divided by {rhs} is {result}");
-        } 
-    }
+}
+class Student: Person{
+
 }
