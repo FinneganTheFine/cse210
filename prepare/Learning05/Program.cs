95a7564fdead5ecac0d252724b3d18ab665614a0
finneganthefine <finneganthefine@gmail.com>
2023-03-02 10:15:40 -0700

    shaoes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        var shapes = new List<Shape>();
+
+        var square = new Square("blue", 5);
+        var rectangle = new Rectangle("red", 5, 4);
+        var circle = new Circle("green", 7);
+
+        shapes.Add(square);
+        shapes.Add(square);
+        shapes.Add(square);
+
+        foreach (var s in shapes){
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+        }
+    }
+}
+abstract class Shape{
+    protected string _color;
+    public Shape(string color){
+        _color = color;
+    }
+
+    public string GetColor(){
+        return _color;
+    }
+    public void SetColor(string color){
+        _color = color;
+    }
+    abstract public double GetArea();
+}
+class Circle: Shape{
+    private double _radius;
+    public Circle(string color, double radius) : base (color){
+        _radius = radius;
+    }
+    public override double GetArea(){
+        return Math.PI * _radius * _radius;
+    }
+}
+class Square: Shape{
+    private double _side;
+    public Square(string color, double side) : base (color){
+        _side = side;
+    }
+    public override double GetArea(){
+        return _side*_side;
+    }
+}
+class Rectangle: Shape{
+    private double _length;
+    private double _width;
+    public Rectangle(string color, double length, double width) : base (color){
+        _length = length;
+        _width = width;
+    }
+    public override double GetArea(){
+        return _length*_width;
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,1 +1,57 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var employees = new List<Employee>();
+
+        var salary = new Employee();
+        var hourly = new HourlyEmployee();
+        var executive = new Executive();
+
+        employees.Add(salary);
+        employees.Add(hourly);
+        employees.Add(executive);
+
+        foreach (var e in employees){
+            e.CalculateYearlyPay();
+        }
+    }
+}
+abstract class Person{
+    abstract public string Speak();
+}
+class NicePerson : Person
+{
+    public override string Speak()
+    {
+        return "Hello! How are you? It's very nice out today";
+    }
+}
+class MeanPerson: Person{
+    public override string Speak()
+    {
+        return "Your code stinks";
+    }
+}
+class Employee{
+    protected int Salary = 100000;
+    virtual public int CalculateYearlyPay(){
+        return Salary;
+    }
+}
+class Executive: Employee{
+    private int Bonus = 100000000;
+    public override int CalculateYearlyPay()
+    {
+        return Salary+Bonus;
+    }
+}
+class HourlyEmployee: Employee{
+    private int HourlyWage = 25;
+    private int HoursPerYear = 2000;
+    public override int CalculateYearlyPay(){
+        return HourlyWage*HoursPerYear;
+    }
+}
