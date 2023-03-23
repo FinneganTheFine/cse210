23ba7eef3ba01a20f30b09206605612bbc914440
finneganthefine <finneganthefine@gmail.com>
2023-03-23 15:02:10 -0600

    I broke something

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,10 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        string action;
+        Program.PerformAction(Console.ReadLine().ToLower());
+    }
+    public static void PerformAction(string action){
+        switch (action){
+            case "go up":
+                break;
+            case "go right":
+                break;
+            case "go down":
+                break;
+            case "go left":
+                break;
+            case "use compass":
+                break;
+            case "use stick":
+                break;
+            case "use sword":
+                if (World.position == 8){
+
+                }
+                break;
+            case "use coin":
+                break;
+            case "use snail":
+                break;
+            case "use knocker":
+                break;
+            case "use well":
+                break;
+        };
     }
 }
-
+public class World{
+    public int position = 4;
+    private List<Map> map = new List<Map>();
+    public World(){
+        map.Add(new Map("name", "description"));
+        map.Add(new Map("name", "description"));
+        map.Add(new Map("name", "description"));
+        map.Add(new Map("name", "description"));
+        map.Add(new Map("name", "description"));
+        map.Add(new Map("name", "description"));
+        map.Add(new Map("name", "description"));
+        map.Add(new Map("name", "description"));
+        map.Add(new Map("name", "description"));
+    }
+}
 class Map{
     private string _name;
     private string _description;
@@ -16,6 +60,11 @@ class Map{
     public Map(string name, string description){
         _name = name;
         _description = description;
+    }
+    public void Display(){
+        Console.WriteLine($"[{_name}]");
+        Console.WriteLine(_description);
+        Console.WriteLine();
     }
 }
 abstract class Items{
@@ -30,7 +79,7 @@ abstract class Items{
 class Compass:Items{
     public Compass(string name, string description):base(name, description){
         _name = "Compass";
-        _description = "";
+        _description = "A small spherical compass, the needle doesn't seem to work quite right";
     }
     public override void UseItem()
     {
@@ -40,7 +89,7 @@ class Compass:Items{
 class Stick:Items{
     public Stick(string name, string description):base(name, description){
         _name = "Stick";
-        _description = "";
+        _description = "It's a stick, the most versatile tool.";
     }
     public override void UseItem()
     {
@@ -50,7 +99,7 @@ class Stick:Items{
 class Sword:Items{
     public Sword(string name, string description):base(name, description){
         _name = "Sword";
-        _description = "";
+        _description = "A weapon of war, could be useful later.";
     }
     public override void UseItem()
     {
@@ -60,7 +109,7 @@ class Sword:Items{
 class Coin:Items{
     public Coin(string name, string description):base(name, description){
         _name = "Coin";
-        _description = "";
+        _description = "Money - Mr.Krabs";
     }
     public override void UseItem()
     {
@@ -68,20 +117,19 @@ class Coin:Items{
     }
 }
 class Snail:Items{
-    private List<string> tips = [
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        ""
-    ];
+    private List<string> tips = new List<string>();
     public Snail(string name, string description):base(name, description){
         _name = "Steven the Snail";
-        _description = "";
+        _description = "A large snail that's wearing a suit, monocle, and tophat.";
+        tips.Add("");
+        tips.Add("");
+        tips.Add("");
+        tips.Add("");
+        tips.Add("");
+        tips.Add("");
+        tips.Add("");
+        tips.Add("");
+        tips.Add("");
     }
     public override void UseItem()
     {
@@ -91,10 +139,20 @@ class Snail:Items{
 class Knocker:Items{
     public Knocker(string name, string description):base(name, description){
         _name = "Knocker";
-        _description = "";
+        _description = "A brass knocker from a large door.";
     }
     public override void UseItem()
     {
         throw new NotImplementedException();
+    }
+}
+class Well:Items{
+    public Well(string name, string description):base(name, description){
+        _name = "Well";
+        _description = "A darkened well, maybe you could draw some water out.";
+    }
+    public override void UseItem()
+    {
+        Console.WriteLine("You cast the bucket into the well and hear it clang against a bunch of coins, the people must have made a lot of wishes.");
     }
 }
